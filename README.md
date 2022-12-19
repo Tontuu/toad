9684555b473271321a35cbca9eee0c14c96a8f2d
Tontuu <tons2.jerry@gmail.com>
2022-12-19 10:35:18 -0300

    [+] Add README and setup script

---
+++ README.md
@@ -0,0 +1,20 @@
+# Toad
+Parse TODO patterns inside a source file
+
+<img src="assets/toad_example.gif">
+
+## Installation
+```
+$ pip install .
+```
+
+## Usage
+```console
+$ toad [OPTIONS...] file
+```
+
+### Some examples:
+
+```
+#
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Toad',
+    version='1.0',
+    author='Tontuu',
+    author_email='my_eleanor.ton@protonmail.com',
+    description='Parse TODO patterns inside a source file',
+    url='https://github.com/Tontuu/MyTon/toad',
+    py_modules = ['toad']
+)
